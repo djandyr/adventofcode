9507005e46b495c5e39960c4c019fb31b1a96dca
djandyr <djandyr@gmail.com>
2021-12-01 21:44:33 +0000

    1 file modified

--- adventofcode.ts
+++ adventofcode.ts
@@ -8,11 +8,10 @@ describe('adventofcode', function () {
     let example: number[] = [199,200,208,210,200,207,240,269,260,263];
     expect(day1(example)).to.equal(7);
 
-    let report  = fs
-    .readFileSync('./src/day1-input.txt', 'utf8')
-    .toString()
-    .trim()
-    .split('\n')
+    let report  = fs.readFile('./src/day1-input.txt', 'utf8' , (err, data) => {
+      console.log(data)
+    })
+  
 
     console.log(report);
   });
