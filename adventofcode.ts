c64d6001a457d37f5c8aed797620d9a3f58d1d51
djandyr <djandyr@gmail.com>
2021-12-01 21:57:47 +0000

    1 file modified

--- adventofcode.ts
+++ adventofcode.ts
@@ -7,11 +7,5 @@ describe('adventofcode', function () {
   it('day1', function () {
     let example: number[] = [199,200,208,210,200,207,240,269,260,263];
     expect(day1(example)).to.equal(7);
-
-    let report  = fs.readFile('./src/day1-input.txt', 'utf8' , (err, data) => {
-      console.log(data)
-    })
-  
-    console.log(report);
   });
 });
