7df2c1c3b361e1b7b40a3904ce2697edd83da9f2
djandyr <djandyr@gmail.com>
2021-12-01 22:02:23 +0000

    1 file modified

--- adventofcode.ts
+++ adventofcode.ts
@@ -4,5 +4,7 @@ describe('adventofcode', function () {
   it('day1', function () {
     let example: number[] = [199,200,208,210,200,207,240,269,260,263];
     expect(day1(example)).to.equal(7);
+
+    const fs = require('fs'); 
   });
 });
