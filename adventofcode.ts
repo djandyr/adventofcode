013a2c2570d46d0be21612444e2170ac48ccbd93
djandyr <djandyr@gmail.com>
2021-12-01 21:32:32 +0000

    1 file modified, 1 added

--- adventofcode.ts
+++ adventofcode.ts
@@ -1,10 +1,19 @@
 import {day1} from './src/day1';
 
+const fs = require('fs');
+const path = require('path');
+
 describe('adventofcode', function () {
   it('day1', function () {
     let example: number[] = [199,200,208,210,200,207,240,269,260,263];
     expect(day1(example)).to.equal(7);
 
-    let report: number[] = [199,200,208,210,200,207,240,269,260,263];
+    let report  = fs
+    .readFileSync(path.join(__dirname, 'day1-input.txt'), 'utf8')
+    .toString()
+    .trim()
+    .split('\n')
+
+    console.log(report);
   });
 });
