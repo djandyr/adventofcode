ea285e6db65c437f026af843fccde65a7410f2a5
djandyr <djandyr@gmail.com>
2021-12-01 21:59:18 +0000

    1 file modified, 1 deleted

--- adventofcode.ts
+++ adventofcode.ts
@@ -1,8 +1,5 @@
 import {day1} from './src/day1';
 
-const fs = require('fs');
-const path = require('path');
-
 describe('adventofcode', function () {
   it('day1', function () {
     let example: number[] = [199,200,208,210,200,207,240,269,260,263];
