deda5d7b64703af4f2d9a517c7a3cafb0d9d1776
Andy Roberts <djandyr@gmail.com>
2021-12-02 01:36:24 +0000

    Completed Day 1: Part 2

--- src/day1.ts
+++ src/day1.ts
@@ -1,10 +1,10 @@
 // --- Day 1: Sonar Sweep ---
-// Function to count number of times a sea level depth increases from previous measurement
-export function day1(depths: Array<number>) {
-  let countIncrease: number = 0;
-  depths.reduce(function(previous, current) {
-    if(current > previous ?? 0) countIncrease += 1;
-    return current;
-  });
-  return countIncrease;
+// Filter number of times a sea level depth increases from previous measurement
+export function filterDepthIncrease(depth, index, report) {
+  return report[index] > report[index - 1];
+}
+
+// Filter by sums of a three-measurement sliding window.
+export function filterDepthIncreaseByThreeSlidingWindowSum(depth, index, report) {
+  return (depth + report[index+1] + report[index + 2]) > (depth + report[index - 1] + report[index + 1]);
 }
--- tests/day1.test.ts
+++ tests/day1.test.ts
@@ -1,13 +1,18 @@
-import {day1} from '../src/day1';
+import {filterDepthIncrease, filterDepthIncreaseByThreeSlidingWindowSum} from '../src/day1';
 import { expect } from 'chai';
 
 describe('adventofcode', function () {
   it('day1', function () {
+
+    // Part One
     let example: number[] = [199,200,208,210,200,207,240,269,260,263];
-    expect(day1(example)).to.equal(7);
+    expect(example.filter(filterDepthIncrease).length).to.equal(7);
 
     const fs = require('fs')
     const input = fs.readFileSync('./tests/fixtures/day1-input.txt', 'utf8').toString().trim().split("\n").map(x => parseInt(x));
-    expect(day1(input)).to.equal(1448);
+    expect(input.filter(filterDepthIncrease).length).to.equal(1448);
+
+    // Part Two
+    expect(input.filter(filterDepthIncreaseByThreeSlidingWindowSum).length).to.equal(1471);
   });
 });
