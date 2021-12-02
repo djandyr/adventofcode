6aa8498c9602414e3e42479d6b54670340bf5792
Andy Roberts <djandyr@gmail.com>
2021-12-02 00:46:53 +0000

    Delete adventofcode.ts

--- adventofcode.ts
+++
@@ -1,8 +0,0 @@
-import {day1} from './src/day1';
-
-describe('adventofcode', function () {
-  it('day1', function () {
-    let example: number[] = [199,200,208,210,200,207,240,269,260,263];
-    expect(day1(example)).to.equal(7);
-  });
-});
