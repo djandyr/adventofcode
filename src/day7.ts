0499bd8d7d199a86f630b7f3fc63439cf50a96c6
Andy Roberts <djandyr@gmail.com>
2021-12-07 23:10:46 +0000

    Day 7 Completed: Refactor variable naming

--- src/day7.ts
+++ src/day7.ts
@@ -10,26 +10,25 @@ export const calculateMinFuel = (input: [], part2 = false) => {
     const minPos = (Math.min(...input))
     const maxPos = (Math.max(...input))
 
-    // Minimum value (fuel, or distance)
-    let minVal = Math.pow(10, 1000) // Initialize with high number
+    let minFuel = Math.pow(10, 1000) // Initialize with high number
 
     for(let pos = minPos; pos < maxPos; pos++) {
         // Total fuel, or disatance
-        let total = 0;
+        let totalFuel = 0;
         for(const crab of input) {
             
             if(part2) {
                 // Part 2: Fuel spent to move by n is the sum of all integers from 1 to n: (1+n)*(n/2)
                 const distance = Math.abs(crab - pos);
-                total += (1 + distance) * distance / 2;
+                totalFuel += (1 + distance) * distance / 2;
             }else{
                 // Part 1
-                total += Math.abs(crab - pos);
+                totalFuel += Math.abs(crab - pos);
             }
         }
 
-        minVal = Math.min(total, minVal);
+        minFuel = Math.min(totalFuel, minFuel);
     }
 
-    return minVal;
+    return minFuel;
 }
