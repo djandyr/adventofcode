2d60f486a5e7bdd2ba8ee6fd316fd4e28d2fbc86
Andy Roberts <djandyr@gmail.com>
2023-12-01 13:29:26 +0000

    AoC 2023: Day One Completed

---
+++ 2023/src/01.py
@@ -0,0 +1,14 @@
+from utils.api import get_input
+from io import StringIO
+import pandas as pd
+
+series = pd.Series(StringIO(get_input(1)))
+def extract_calibration_value(s):
+    digits = ''.join(filter(str.isdigit, s))
+    return int(digits[0] + digits[-1]) if digits else 0
+
+# Part One
+print(series.str.findall('\d').apply(extract_calibration_value).sum())
+# Part Two
+num_dict = {'zero': 'z0o', 'one': 'o1e', 'two': 't2o', 'three': 't3e', 'four': 'f4r', 'five': 'f5e', 'six': 's6x', 'seven': 's7n', 'eight': 'e8t', 'nine': 'n9e'}
+print(series.replace(num_dict, regex=True).str.findall('\d').apply(extract_calibration_value).sum())
